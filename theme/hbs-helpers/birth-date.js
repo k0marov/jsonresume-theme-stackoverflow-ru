d62fc9126e73d8e7629c21cec5e583e8b93fe6bb
Sam Komarov <sam@skomarov.com>
2024-03-23 10:49:03 +0300

    add russian locale to dates

--- theme/hbs-helpers/birth-date.js
+++ theme/hbs-helpers/birth-date.js
@@ -1,20 +1,22 @@
 const moment = require('moment');
 const { SafeString } = require('handlebars');
 
+moment.locale('ru'); // set russian locale globally
+
 const birthDate = (birth) => {
   const out = [];
   if (birth && Object.keys(birth).length) {
     if (birth.place) {
-      out.push(`<div> Born in ${birth.place}`);
+      out.push(`<div> Родился в ${birth.place}`);
     }
     if (birth.place && birth.state) {
       out.push(`, ${birth.state}`);
     }
     const year = birth.date ? moment(birth.date.toString(), ['YYYY-MM-DD']).format('YYYY') : '';
     if (year && birth.place && birth.state) {
-      out.push(` in ${year}</div>`);
+      out.push(` в ${year}</div>`);
     } else if (year && (!birth.place || birth.state)) {
-      out.push(`<div> Born in ${year}</div>`);
+      out.push(`<div> Родился в ${year}</div>`);
     }
   }
 
